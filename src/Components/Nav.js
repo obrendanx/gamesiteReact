0269d39fce05603b9956730afd5de5f550e2efd2
Brendan <brendanewen@gmail.com>
2021-05-25 13:40:30 +0100

    changed color scheme

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../images/logo.jpg'
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,13 +4,13 @@ import LgTextComp from '../Components/LgTextComp'
 import SmTextComp from '../Components/SmTextComp'
 
 import RDRDUpdate from '../images/rdrdupdate.jpg'
-import SW from '../images/main/sw.webp'
-import CODCW from '../images/main/codcw.jpg'
+import SW from '../images/main/sw.jpg'
+import MARIO from '../images/main/mario.webp'
 import GAMESCORE from '../images/gamescore.jpg'
 import GAMEPC from '../images/gamepc.webp'
 import SWITCH from '../images/switch.webp'
 import FALLOUT from '../images/falloutsd.jpg'
-import MINIGONE from '../images/minigone.jpg'
+import MINECRAFT from '../images/main/minecraft.png'
 import WARFRAME from '../images/Warframe.jpg'
 
 var comp_one_text = "Some information about the image, and about new updates that are out. Learn more";
@@ -24,15 +24,15 @@ var comp_smtwo_title = "Main Title 2";
 
 function Home() {
     return (
-        <div>
+        <div className="home_wrap">
             <Banner />
             <div className="text_comp_one">
                 <div className="large_textbox_wrap">
                     <LgTextComp imgName={SW} imgText={comp_one_text} imgTitle={comp_one_title}/>
                 </div>
                 <div className="small_textbox_wrap">
-                    <SmTextComp imgName={GAMESCORE} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
-                    <SmTextComp imgName={CODCW} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
+                    <SmTextComp imgName={MINECRAFT} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
+                    <SmTextComp imgName={MARIO} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
                 </div>
             </div>
 
