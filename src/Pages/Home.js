a055aa0e032dc3d00ae5f70eebb4ae796b6cfe9d
Brendan <brendanewen@gmail.com>
2021-05-27 12:35:37 +0100

    updated imagesand resposiveness for home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,15 +3,15 @@ import Banner from '../Components/Banner'
 import LgTextComp from '../Components/LgTextComp'
 import SmTextComp from '../Components/SmTextComp'
 
-import RDRDUpdate from '../images/rdrdupdate.jpg'
+import DPREMAKE from '../images/main/dpremake.webp'
 import SW from '../images/main/sw.jpg'
 import MARIO from '../images/main/mario.webp'
-import GAMESCORE from '../images/gamescore.jpg'
-import GAMEPC from '../images/gamepc.webp'
-import SWITCH from '../images/switch.webp'
-import FALLOUT from '../images/falloutsd.jpg'
+import ZELDA from '../images/main/zelda.webp'
+import PS5 from '../images/main/ps5.jpg'
+import XBOX from '../images/main/xboxx.jpg'
+import SMASH from '../images/main/smashbro.jpg'
 import MINECRAFT from '../images/main/minecraft.png'
-import WARFRAME from '../images/Warframe.jpg'
+import AC from '../images/main/animalcrossing.jpg'
 
 var comp_one_text = "Some information about the image, and about new updates that are out. Learn more";
 var comp_one_title = "Main Title";
@@ -36,17 +36,21 @@ function Home() {
                 </div>
             </div>
 
+            <h2 className="section_header">Nintendo</h2>
+
             <div className="text_comp_two">
                     <div className="comp_two_wrapone">
-                        <SmTextComp imgName={GAMESCORE} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
-                        <SmTextComp imgName={WARFRAME} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
-                        <SmTextComp imgName={RDRDUpdate} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
-                        <SmTextComp imgName={FALLOUT} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
+                        <SmTextComp imgName={ZELDA} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
+                        <SmTextComp imgName={AC} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
+                        <SmTextComp imgName={DPREMAKE} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
+                        <SmTextComp imgName={SMASH} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
                     </div>
 
+                    <h2 className="section_header">Top Stories</h2>
+
                     <div className="comp_two_wraptwo">
-                        <LgTextComp imgName={GAMEPC} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
-                        <LgTextComp imgName={SWITCH} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
+                        <LgTextComp imgName={PS5} imgText={comp_smone_text} imgTitle={comp_smone_title}/>
+                        <LgTextComp imgName={XBOX} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title}/>
                     </div>
                 </div>
         </div>
