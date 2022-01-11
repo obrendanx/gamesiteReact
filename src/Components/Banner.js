69cbd68c99d5c6e723d26be0284008e4c4e33b14
Brendan Ewen <brendanewen@gmail.com>
2022-01-11 23:35:58 +0000

    - added different imgs for home
    - home finished may need to adjust reponsiveness a little

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,6 +3,7 @@ import "react-toggle/style.css" // for ES6 modules
 import { View, Text, TouchableHighlight, TouchableOpacity, TextInput, Image, ImageBackground } from 'react-native'
 import StyleSheet from 'react-native-media-query';
 
+//importing images for the banner slideshow
 import RDRDUpdate from '../images/rdrdupdate.jpg'
 import REACTGaming from '../images/react-gaming.jpeg'
 import WARZONE from '../images/warzone.jpg'
@@ -11,7 +12,7 @@ import BANOne from '../images/banone.jpg'
 import BANTwo from '../images/bantwo.webp'
 import BANThree from '../images/banthree.jpg'
 
-
+//creating the array for the banner images
 export const imgArr = [
   REACTGaming,
   BANOne,
@@ -22,6 +23,7 @@ export const imgArr = [
 class banner extends Component {
     constructor(props) {
         super(props);
+        //couunter for banner slideshow
         this.state = {
           counter: 0,
         }
@@ -29,6 +31,7 @@ class banner extends Component {
 
       componentDidMount() {
         this.interval = setInterval(() => {
+          //updating counter by one every minute
           this.setState({ counter: this.state.counter + 1 })
           console.log(this.state.counter);
         }, 10000)
@@ -39,10 +42,13 @@ class banner extends Component {
       }
 
       changeImage(button) {
+        //check what button is pressed left or right
         if(button == 1) {
+          // add one if right is pushed
           if (this.state.counter == imgArr.length - 1) this.setState({counter: 0})
           else this.setState({counter: this.state.counter + 1})
         } else {
+          //minus one if left is pushed
           if (this.state.counter == 0) this.setState({counter: imgArr.length - 1})
           else this.setState({counter: this.state.counter - 1}) 
         }
@@ -61,12 +67,15 @@ class banner extends Component {
         button1=true;
         } 
 
+        // if counter becomes greater than amount in banner reset
         if(this.state.counter > 3){
           this.setState({
             counter: 0
           });
         }
 
+
+        //setting text for each image depending on counter state 
         if(this.state.counter == 0){
           imgTitle = "React Gaming";
           imgMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in maximus sapien. Duis eget pulvinar massa, quis varius libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam sit amet libero vitae metus placerat efficitur. Curabitur nec molestie sem";
@@ -144,11 +153,14 @@ class banner extends Component {
         return (
             <div>
                 <View>
+                    
+                    {/*displaying main image for banner*/}
                     <ImageBackground source = {imgArr[this.state.counter]} 
                         style={styles.ban}
                         dataSet={{ media: ids.ban}}
                     >
                       <div className="touchFlex">
+                      {/*arrow key left*/}
                     <TouchableHighlight 
                         disabled={button2} 
                         onPress = {(e) => this.changeImage(2)}
@@ -163,7 +175,7 @@ class banner extends Component {
                           &lt;
                       </Text>
                       </TouchableHighlight>
-
+                      {/*arrow key right*/}
                       <TouchableHighlight 
                         disabled={button1} 
                         onPress = {(e) => this.changeImage(1)}
@@ -194,6 +206,7 @@ class banner extends Component {
                             padding: "2.5%"
                           }}
                         >
+                          {/* adding text to the banner image */}
                             <header>
                               <h1
                                 style={{
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -13,6 +13,17 @@ import XBOX from '../images/main/xboxx.jpg'
 import SMASH from '../images/main/smashbro.jpg'
 import MINECRAFT from '../images/main/minecraft.png'
 import AC from '../images/main/animalcrossing.jpg'
+import CODMOB from '../images/main/codmob.jpeg'
+import HALOINFINITE from '../images/main/haloinfinite.jpeg'
+import PKMPEARL from '../images/main/pkmpearl.jpeg'
+import FALLOUT76 from '../images/main/fallout76.jpeg'
+import CYBERPUNK from '../images/main/cyberpunk.jpeg'
+import SPIDERMAN2 from '../images/main/spiderman2.png'
+import WARFRAMENEWWAR from '../images/main/warframenewwar.jpeg'
+import PKMDIAMOND from '../images/main/pkmdiamond.jpeg'
+import PKMFIRERED from '../images/main/pkmfirered.jpg'
+import PKMBLACK from '../images/main/pkmblack.jpeg'
+import PKMWHITE2 from '../images/main/pkmwhite2.jpg'
 
 var comp_one_text = "Some information about the image, and about new updates that are out. Learn more";
 var comp_one_title = "Main Title";
@@ -60,37 +71,37 @@ function Home() {
 
                     <div className="text_comp_one">
                         <div className="small_textbox_wrap">
-                            <MdTextComp imgName={MINECRAFT} imgText={comp_smone_text} imgTitle={comp_smone_title} margbot={"margbot"}/>
-                            <MdTextComp imgName={MARIO} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title} margbot={"margbot"}/>
+                            <MdTextComp imgName={CODMOB} imgText={comp_smone_text} imgTitle={comp_smone_title} margbot={"margbot"}/>
+                            <MdTextComp imgName={PKMPEARL} imgText={comp_smtwo_text} imgTitle={comp_smtwo_title} margbot={"margbot"}/>
                         </div>
                         <div className="large_textbox_wrap">
-                            <LgTextComp imgName={SW} imgText={comp_smone_text} imgTitle={comp_one_title}/>
+                            <LgTextComp imgName={HALOINFINITE} imgText={comp_smone_text} imgTitle={comp_one_title}/>
                         </div>
                     </div>
 
                     <div className="text_comp_one">
                     <div className="large_textbox_wrap">
-                            <LgTextComp imgName={SW} imgText={comp_smone_text} imgTitle={comp_one_title}/>
+                            <LgTextComp imgName={CYBERPUNK} imgText={comp_smone_text} imgTitle={comp_one_title}/>
                         </div>
                         <div className="large_textbox_wrap">
-                            <LgTextComp imgName={SW} imgText={comp_smone_text} imgTitle={comp_one_title}/>
+                            <LgTextComp imgName={FALLOUT76} imgText={comp_smone_text} imgTitle={comp_one_title}/>
                         </div>
                     </div>
 
                     <div className="text_comp_one">
                     <div className="large_textbox_wrap">
-                            <LgTextComp imgName={SW} imgText={comp_smone_text} imgTitle={comp_one_title}/>
+                            <LgTextComp imgName={SPIDERMAN2} imgText={comp_smone_text} imgTitle={comp_one_title}/>
                         </div>
                         <div className="large_textbox_wrap">
-                            <LgTextComp imgName={SW} imgText={comp_smone_text} imgTitle={comp_one_title}/>
+                            <LgTextComp imgName={WARFRAMENEWWAR} imgText={comp_smone_text} imgTitle={comp_one_title}/>
                         </div>
                     </div>
 
                     <div className="comp_two_wrapone">
-                        <SmTextComp imgName={ZELDA} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s1'}/>
-                        <SmTextComp imgName={AC} imgText={comp_one_text} imgTitle={comp_smtwo_title} colorscheme={'color-scheme-s2'}/>
-                        <SmTextComp imgName={DPREMAKE} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s3'}/>
-                        <SmTextComp imgName={SMASH} imgText={comp_one_text} imgTitle={comp_smtwo_title} colorscheme={'color-scheme-s4'}/>
+                        <SmTextComp imgName={PKMBLACK} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s1'}/>
+                        <SmTextComp imgName={PKMDIAMOND} imgText={comp_one_text} imgTitle={comp_smtwo_title} colorscheme={'color-scheme-s2'}/>
+                        <SmTextComp imgName={PKMFIRERED} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s3'}/>
+                        <SmTextComp imgName={PKMWHITE2} imgText={comp_one_text} imgTitle={comp_smtwo_title} colorscheme={'color-scheme-s4'}/>
                         {/* <SmTextComp imgName={ZELDA} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s1'}/>
                         <SmTextComp imgName={AC} imgText={comp_one_text} imgTitle={comp_smtwo_title} colorscheme={'color-scheme-s2'}/>
                         <SmTextComp imgName={DPREMAKE} imgText={comp_one_text} imgTitle={comp_smone_title} colorscheme={'color-scheme-s3'}/>
