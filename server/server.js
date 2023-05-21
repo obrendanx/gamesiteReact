d0a6c6aab4235b297570175e3c67ad06b2bdf5d0
brendan <brendanewen16@outlook.com>
2023-05-21 14:55:41 +0100

    feature: Implemented the ability for a post to be added to the database

--- server/server.js
+++ server/server.js
@@ -3,23 +3,36 @@ require("dotenv").config({ path: "./.env" });
 
 const express = require("express");
 const app = express();
-
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const recordUrls = require('./routes/record')
+const recordUrl = require('./routes/userRecord')
+const postUrl = require('./routes/post')
 const cors = require('cors')
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database Connected"))
+mongoose.connect(process.env.DATABASE_ACCESS, {
+  dbName: 'gamesite',
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB cluster!');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
   app.use(express.json())
   const bodyParser = require('body-parser');
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cors())
-  app.use('/app', recordUrls)
+  app.use('/app', recordUrl)
+  app.use('/app', postUrl)
+  
+
   // start the Express server
-  app.listen(4200, () => {
+  app.listen(5000, () => {
     console.log(`Server is running on port: 5000`);
   });
 
--- src/Components/user/Post.js
+++ src/Components/user/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
 
 const Wrapper = styled.div`
