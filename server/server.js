dd166745a40998623e8322a4491e37e831a79912
Brendan <brendanewen16@outlook.com>
2023-08-05 11:29:30 +0100

    build: made some server routing changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv')
 const recordUrl = require('./routes/userRecord')
 const postUrl = require('./routes/post')
 const cors = require('cors')
+const path = require('path');
 
 dotenv.config()
 
@@ -23,22 +24,24 @@ mongoose.connect(process.env.DATABASE_ACCESS, {
     console.error('Error connecting to MongoDB:', error);
   });
 
-  app.use(express.json())
-  const bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cors(
-    {
-      origin: ['https://bewen.net']
-    }
-  ));
-  app.use('/app', recordUrl)
-  app.use('/app', postUrl)
-  
+app.use(express.json())
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(
+  {
+    origin: ['https://bewen.net']
+  }
+));
+app.use('/app', recordUrl)
+app.use('/app', postUrl)
 
-  // start the Express server
-  app.listen(5000, () => {
-    console.log(`Server is running on port: 5000`);
-  });
+// Catch-all route to serve the React app for any route
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-
-  
+// Start the Express server
+app.listen(5000, () => {
+  console.log(`Server is running on port: 5000`);
+});
