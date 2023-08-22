d9b5c165693f4d50698ee34fcc4f5ad5c264a72d
Brendan <brendanewen16@outlook.com>
2023-08-22 14:08:46 +0100

    build: axios url change

--- src/Components/User/Profile/User.js
+++ src/Components/User/Profile/User.js
@@ -154,7 +154,7 @@ function User() {
     const fetchUserFavourites = async () => {
       try {
         const response = await axios.get(
-          `http://localhost:5000/app/userfavourites/${username}`
+          `https://gamesite-backend.onrender.com/app/userfavourites/${username}`
         );
         setFavourites(response.data);
       } catch (error) {
