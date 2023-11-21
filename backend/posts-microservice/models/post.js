2735a2415aefeae82a6563edfdf6f68699349f2b
brendan <brendanewen16@outlook.com>
2023-11-21 11:49:21 +0000

    feature: updated post model to add like and dislike totals as well as liked users

--- backend/posts-microservice/models/post.js
+++ backend/posts-microservice/models/post.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose')
 
-const fourmPost = new mongoose.Schema({
-  subject:{
-    type:String,
-    required:true
+const forumPostSchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true
   },
-  message:{
-    type:String,
-    required:true,
+  message: {
+    type: String,
+    required: true,
   },
-  postedBy:{
-    type:String,
-    required:true
+  postedBy: {
+    type: String,
+    required: true
   },
-  date:{
-    type:Date,
-    default:Date.now
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  likeTotal: {
+    type: Number,
+    default: 0
+  },
+  dislikeTotal: {
+    type: Number,
+    default: 0
+  },
+  usersWhoLiked: {
+    type: [String], // Array of user IDs who have liked the post
+    default: []
   }
 })
 
-module.exports = mongoose.model('posts', fourmPost)
+module.exports = mongoose.model('posts', forumPostSchema)
